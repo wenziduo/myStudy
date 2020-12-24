a65423e477a5fbeb110b950432a1cf143c050cca
苡仁（蔡雯多） <caiwenduo@weimeng-hosp.com>
2020-12-24 18:47:16 +0800

    Auto-commit

--- binStudy/bin/ao.js
+++ binStudy/bin/ao.js
@@ -1,20 +1,22 @@
-const http = require('http');
+const http = require("http");
+const modulesA = require("./modulesA");
+console.log("modulesA", modulesA);
 const longComputation = () => {
- let sum = 0;
- for (let i = 0; i < 1e10; i++) {
- sum += i;
- };
- return sum;
+  let sum = 0;
+  for (let i = 0; i < 1e10; i++) {
+    sum += i;
+  }
+  return sum;
 };
 const server = http.createServer();
-server.on('request', (req, res) => {
- if (req.url === '/compute') {
- console.info('计算开始',new Date());
- const sum = longComputation();
- console.info('计算结束',new Date());
- return res.end(`Sum is ${sum}`);
- } else {
- res.end('Ok')
- }
+server.on("request", (req, res) => {
+  if (req.url === "/compute") {
+    console.info("计算开始", new Date());
+    const sum = longComputation();
+    console.info("计算结束", new Date());
+    return res.end(`Sum is ${sum}`);
+  } else {
+    res.end("Ok");
+  }
 });
 server.listen(3000);
